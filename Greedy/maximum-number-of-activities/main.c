4472f3610890dd9894d31a9be59536366261a8f5
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-05-20 17:27:35 +0300

    Add basic functions

--- Greedy/maximum-number-of-activities/main.c
+++ Greedy/maximum-number-of-activities/main.c
@@ -1,5 +1,47 @@
 #include <stdio.h>
 
+typedef struct
+{
+    int StartTime, EndTime;
+    char ActivityName[100];
+} Activity;
+
+int GetNumberOfActivities(int *NumberOfActivities)
+{
+    printf("Number of activities : ");
+    scanf("%d", NumberOfActivities);
+
+    return (*NumberOfActivities);
+}
+
+void GetActivities(int NumberOfActivities, Activity *Activities)
+{
+    for (int i = 0; i < NumberOfActivities; ++i)
+    {
+        scanf("%d %d", &Activities[i].StartTime, &Activities[i].EndTime);
+        gets(Activities[i].ActivityName);
+    }
+}
+
+void SortActivities(Activity * Activities, int NumberOfActivities)
+{
+    for (int i = 0; i < NumberOfActivities; ++i)
+        for (int j = i + 1; j < NumberOfActivities; ++j)
+            if (Activities[i].StartTime > Activities[j].StartTime)
+            {
+                Activity Auxiliary = Activities[i];
+                Activities[i] = Activities[j];
+                Activities[j] = Auxiliary;
+            }
+}
+
+void ShowSelectedActivities(Activity * Activities, int * SelectedActivities, int NumberOfSelectedActivities)
+{
+    for (int i = 0; i < NumberOfSelectedActivities; ++i)
+        printf("%d %d %d %s\n", SelectedActivities[i], Activities[SelectedActivities[i]].StartTime,
+               Activities[SelectedActivities[i]].EndTime, Activities[SelectedActivities[i]].ActivityName);
+}
+
 int main()
 {
 
