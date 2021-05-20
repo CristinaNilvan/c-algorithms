b89a028480731f7fd764a02866271a351b052be1
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-05-20 17:26:25 +0300

    Create maximum-number-of-activities

---
+++ Greedy/maximum-number-of-activities/cmake-build-debug/CMakeFiles/maximum_number_of_activities.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maximum_number_of_activities.dir/main.c.obj"
+  "libmaximum_number_of_activities.dll.a"
+  "maximum_number_of_activities.exe"
+  "maximum_number_of_activities.exe.manifest"
+  "maximum_number_of_activities.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/maximum_number_of_activities.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Greedy/maximum-number-of-activities/cmake-build-debug/CMakeFiles/maximum_number_of_activities.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for maximum_number_of_activities.
+# This may be replaced when dependencies are built.
