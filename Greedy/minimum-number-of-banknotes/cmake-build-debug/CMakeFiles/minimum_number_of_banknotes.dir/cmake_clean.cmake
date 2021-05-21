35791d8e84f4540dc5d94b5cf65e6e4da326be4b
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-05-21 14:15:39 +0300

    Create minimum-number-of-banknotes

---
+++ Greedy/minimum-number-of-banknotes/cmake-build-debug/CMakeFiles/minimum_number_of_banknotes.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minimum_number_of_banknotes.dir/main.c.obj"
+  "libminimum_number_of_banknotes.dll.a"
+  "minimum_number_of_banknotes.exe"
+  "minimum_number_of_banknotes.exe.manifest"
+  "minimum_number_of_banknotes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/minimum_number_of_banknotes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Greedy/minimum-number-of-banknotes/cmake-build-debug/CMakeFiles/minimum_number_of_banknotes.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for minimum_number_of_banknotes.
+# This may be replaced when dependencies are built.
