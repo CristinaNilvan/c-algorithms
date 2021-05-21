a8c4683b78c7867f95d89d51dff6b398ed9bc381
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-05-21 14:17:44 +0300

    Add solution

--- Greedy/minimum-number-of-banknotes/main.c
+++ Greedy/minimum-number-of-banknotes/main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 
+int FindMinimumNumberOfBanknotes(int AmountOfMoney, int * TypesOfBanknotes, int NumberOfBanknoteTypes)
+{
+    int NumberOfBanknotes = 0;
+
+    for (int i = NumberOfBanknoteTypes - 1; i >= 0; i--)
+        if (AmountOfMoney >= TypesOfBanknotes[i])
+        {
+            int BanknotesOfTheSameType = AmountOfMoney / TypesOfBanknotes[i];
+            printf("%d x %d     ", BanknotesOfTheSameType, TypesOfBanknotes[i]);
+
+            NumberOfBanknotes += BanknotesOfTheSameType;
+            AmountOfMoney -= BanknotesOfTheSameType * TypesOfBanknotes[i];
+        }
+
+    printf("\n");
+
+    return NumberOfBanknotes;
+}
+
+void TestsFindingMinimumNumberOfBanknotes()
+{
+    int NumberOfBanknoteTypes = 7;
+    int TypesOfBanknotes[] = {1, 5, 10, 50, 100, 200, 500};
+
+    int NumberOfAmountsOfMoney = 5;
+    int AmountsOfMoney[] = {23, 56, 70, 102, 15};
+
+    for (int i = 0; i < NumberOfAmountsOfMoney; i++)
+    {
+        printf("The amount of money %d decomposed is : ", AmountsOfMoney[i]);
+        printf("Number of banknotes used to decompose = %d.\n",
+               FindMinimumNumberOfBanknotes(AmountsOfMoney[i],TypesOfBanknotes, NumberOfBanknoteTypes));
+    }
+}
+
 int main()
 {
-    
+    TestsFindingMinimumNumberOfBanknotes();
+
     return 0;
 }
