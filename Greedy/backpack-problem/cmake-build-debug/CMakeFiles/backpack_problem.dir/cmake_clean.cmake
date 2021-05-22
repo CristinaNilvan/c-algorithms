e58a44812bb62bf5d0f1a8386ba5f54052c6871a
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-05-22 19:12:42 +0300

    Create backpack-problem

---
+++ Greedy/backpack-problem/cmake-build-debug/CMakeFiles/backpack_problem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/backpack_problem.dir/main.c.obj"
+  "backpack_problem.exe"
+  "backpack_problem.exe.manifest"
+  "backpack_problem.pdb"
+  "libbackpack_problem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/backpack_problem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Greedy/backpack-problem/cmake-build-debug/CMakeFiles/backpack_problem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for backpack_problem.
+# This may be replaced when dependencies are built.
