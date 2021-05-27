3aa314d1d0d16608b6cc7e61f8d2747149098389
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-05-27 14:09:13 +0300

    Create binary-search

---
+++ Divide and Conquer/binary-search/cmake-build-debug/CMakeFiles/binary_search.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binary_search.dir/main.c.obj"
+  "binary_search.exe"
+  "binary_search.exe.manifest"
+  "binary_search.pdb"
+  "libbinary_search.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/binary_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Divide and Conquer/binary-search/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int GetNumberOfElements()
+{
+    int NumberOfElements;
+
+    printf("Number of elements = ");
+    scanf("%d", &NumberOfElements);
+
+    return NumberOfElements;
+}
+
+void ReadElements(int * Array, int NumberOfElements)
+{
+    for (int i = 0; i < NumberOfElements; ++i)
+        scanf("%d", &Array[i]);
+}
+
+int GetSearchedNumber()
+{
+    int SearchedNumber;
+
+    printf("Searched number = ");
+    scanf("%d", &SearchedNumber);
+
+    return SearchedNumber;
+}
+
+bool BinarySearch(int * Array, int Left, int Right, int SearchedNumber)
+{
+    if (Left <= Right)
+    {
+        int Mid = (Left + Right) / 2;
+
+        if (Array[Mid] == SearchedNumber)
+            return true;
+
+        if (Array[Mid] < SearchedNumber)
+            return BinarySearch(Array, Mid + 1, Right, SearchedNumber);
+        else
+            return BinarySearch(Array, Left, Mid + 1, SearchedNumber);
+    }
+
+    return false;
+}
+
+void TestsBinarySearch(int NumberOfElements, int * Array, int SearchedNumber)
+{
+    bool Found = BinarySearch(Array, 0, NumberOfElements, SearchedNumber);
+
+    if (Found)
+        printf("The number %d has been found. ", SearchedNumber);
+    else
+        printf("The number %d has not been found. ", SearchedNumber);
+}
+
+int main()
+{
+    int NumberOfElements = GetNumberOfElements();
+
+    int Array[NumberOfElements];
+    ReadElements(Array, NumberOfElements);
+
+    int SearchedNumber = GetSearchedNumber();
+    TestsBinarySearch(NumberOfElements, Array, SearchedNumber);
+
+    return 0;
+}
