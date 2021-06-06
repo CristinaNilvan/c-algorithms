a8bc1857178c084ed9cbf230285a911725c6dc38
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-06-06 22:12:17 +0300

    Create backpack-problem-with-tree-nodes

---
+++ Greedy/backpack-problem-with-tree-nodes/cmake-build-debug/CMakeFiles/backpack_problem_with_tree_nodes.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/backpack_problem_with_tree_nodes.dir/main.c.obj"
+  "backpack_problem_with_tree_nodes.exe"
+  "backpack_problem_with_tree_nodes.exe.manifest"
+  "backpack_problem_with_tree_nodes.pdb"
+  "libbackpack_problem_with_tree_nodes.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/backpack_problem_with_tree_nodes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Greedy/backpack-problem-with-tree-nodes/cmake-build-debug/CMakeFiles/backpack_problem_with_tree_nodes.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for backpack_problem_with_tree_nodes.
+# This may be replaced when dependencies are built.
